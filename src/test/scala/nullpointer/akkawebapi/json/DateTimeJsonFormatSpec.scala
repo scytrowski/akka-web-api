816934cf9a2a57c78029c7fce35e34efd540b7e0
vdrake6 <vdrake6@gmail.com>
2018-08-21 18:40:27 +0200

    DateTimeJsonFormat tests refactor

--- src/test/scala/nullpointer/akkawebapi/json/DateTimeJsonFormatSpec.scala
+++ src/test/scala/nullpointer/akkawebapi/json/DateTimeJsonFormatSpec.scala
@@ -1,191 +1,119 @@
 package nullpointer.akkawebapi.json
 
 import org.joda.time.DateTime
-import org.scalatest.{FunSpec, MustMatchers}
-import spray.json.{BasicFormats, JsNumber, JsObject}
+import spray.json._
 
 class DateTimeJsonFormatSpec extends JsonFormatSpec {
+  import DateTimeJsonFormatSpec._
+
   describe("A DateTimeJsonFormat") {
     it("must write correct year value") {
-      val format = DateTimeJsonFormat()
-      val expectedYear = 2015
-      val dateTime = new DateTime(expectedYear, 10, 25, 19, 25, 38, 150)
-      val json = format.write(dateTime).asJsObject
-      val year = json.fields.get("year").map(_.convertTo[Int])
-      year.isDefined mustBe true
-      year.get mustBe expectedYear
+      val json = dateTimeToJson(TestDateTime)
+      json.intField("year") mustBe Some(Year)
     }
 
     it("must write correct month of year value") {
-      val format = DateTimeJsonFormat()
-      val expectedMonthOfYear = 10
-      val dateTime = new DateTime(2015, expectedMonthOfYear, 25, 19, 25, 38, 150)
-      val json = format.write(dateTime).asJsObject
-      val monthOfYear = json.fields.get("monthOfYear").map(_.convertTo[Int])
-      monthOfYear.isDefined mustBe true
-      monthOfYear.get mustBe expectedMonthOfYear
+      val json = dateTimeToJson(TestDateTime)
+      json.intField("monthOfYear") mustBe Some(MonthOfYear)
     }
 
     it("must write correct day of month value") {
-      val format = DateTimeJsonFormat()
-      val expectedDayOfMonth = 25
-      val dateTime = new DateTime(2015, 10, expectedDayOfMonth, 19, 25, 38, 150)
-      val json = format.write(dateTime).asJsObject
-      val dayOfMonth = json.fields.get("dayOfMonth").map(_.convertTo[Int])
-      dayOfMonth.isDefined mustBe true
-      dayOfMonth.get mustBe expectedDayOfMonth
+      val json = dateTimeToJson(TestDateTime)
+      json.intField("dayOfMonth") mustBe Some(DayOfMonth)
     }
 
     it("must write correct hour of day value") {
-      val format = DateTimeJsonFormat()
-      val expectedHourOfDay = 19
-      val dateTime = new DateTime(2015, 10, 25, expectedHourOfDay, 25, 38, 150)
-      val json = format.write(dateTime).asJsObject
-      val hourOfDay = json.fields.get("hourOfDay").map(_.convertTo[Int])
-      hourOfDay.isDefined mustBe true
-      hourOfDay.get mustBe expectedHourOfDay
+      val json = dateTimeToJson(TestDateTime)
+      json.intField("hourOfDay") mustBe Some(HourOfDay)
     }
 
     it("must write correct minute of hour value") {
-      val format = DateTimeJsonFormat()
-      val expectedMinuteOfHour = 25
-      val dateTime = new DateTime(2015, 10, 25, 19, expectedMinuteOfHour, 38, 150)
-      val json = format.write(dateTime).asJsObject
-      val minuteOfHour = json.fields.get("minuteOfHour").map(_.convertTo[Int])
-      minuteOfHour.isDefined mustBe true
-      minuteOfHour.get mustBe expectedMinuteOfHour
+      val json = dateTimeToJson(TestDateTime)
+      json.intField("minuteOfHour") mustBe Some(MinuteOfHour)
     }
 
     it("must write correct second of minute value") {
-      val format = DateTimeJsonFormat()
-      val expectedSecondOfMinute = 38
-      val dateTime = new DateTime(2015, 10, 25, 19, 25, expectedSecondOfMinute, 150)
-      val json = format.write(dateTime).asJsObject
-      val secondOfMinute = json.fields.get("secondOfMinute").map(_.convertTo[Int])
-      secondOfMinute.isDefined mustBe true
-      secondOfMinute.get mustBe expectedSecondOfMinute
+      val json = dateTimeToJson(TestDateTime)
+      json.intField("secondOfMinute") mustBe Some(SecondOfMinute)
     }
 
     it("must write correct millis of second value") {
-      val format = DateTimeJsonFormat()
-      val expectedMillisOfSecond = 150
-      val dateTime = new DateTime(2015, 10, 25, 19, 25, 38, expectedMillisOfSecond)
-      val json = format.write(dateTime).asJsObject
-      val millisOfSecond = json.fields.get("millisOfSecond").map(_.convertTo[Int])
-      millisOfSecond.isDefined mustBe true
-      millisOfSecond.get mustBe expectedMillisOfSecond
+      val json = dateTimeToJson(TestDateTime)
+      json.intField("millisOfSecond") mustBe Some(MillisOfSecond)
     }
 
     it("must read correct year value") {
-      val format = DateTimeJsonFormat()
-      val expectedYear = 2015
-      val json = JsObject(
-        "year" -> JsNumber(expectedYear),
-        "monthOfYear" -> JsNumber(10),
-        "dayOfMonth" -> JsNumber(25),
-        "hourOfDay" -> JsNumber(19),
-        "minuteOfHour" -> JsNumber(25),
-        "secondOfMinute" -> JsNumber(38),
-        "millisOfSecond" -> JsNumber(150)
-      )
-      val dateTime = format.read(json)
-      dateTime.getYear mustBe expectedYear
+      val dateTime = jsonToDateTime(TestDateTimeJsonObject)
+      dateTime.getYear mustBe Year
     }
 
     it("must read correct month of year value") {
-      val format = DateTimeJsonFormat()
-      val expectedMonthOfYear = 10
-      val json = JsObject(
-        "year" -> JsNumber(2015),
-        "monthOfYear" -> JsNumber(expectedMonthOfYear),
-        "dayOfMonth" -> JsNumber(25),
-        "hourOfDay" -> JsNumber(19),
-        "minuteOfHour" -> JsNumber(25),
-        "secondOfMinute" -> JsNumber(38),
-        "millisOfSecond" -> JsNumber(150)
-      )
-      val dateTime = format.read(json)
-      dateTime.getMonthOfYear mustBe expectedMonthOfYear
+      val dateTime = jsonToDateTime(TestDateTimeJsonObject)
+      dateTime.getMonthOfYear mustBe MonthOfYear
     }
 
     it("must read correct day of month value") {
-      val format = DateTimeJsonFormat()
-      val expectedDayOfMonth = 25
-      val json = JsObject(
-        "year" -> JsNumber(2015),
-        "monthOfYear" -> JsNumber(10),
-        "dayOfMonth" -> JsNumber(expectedDayOfMonth),
-        "hourOfDay" -> JsNumber(19),
-        "minuteOfHour" -> JsNumber(25),
-        "secondOfMinute" -> JsNumber(38),
-        "millisOfSecond" -> JsNumber(150)
-      )
-      val dateTime = format.read(json)
-      dateTime.getDayOfMonth mustBe expectedDayOfMonth
+      val dateTime = jsonToDateTime(TestDateTimeJsonObject)
+      dateTime.getDayOfMonth mustBe DayOfMonth
     }
 
     it("must read correct hour of day value") {
-      val format = DateTimeJsonFormat()
-      val expectedHourOfDay = 19
-      val json = JsObject(
-        "year" -> JsNumber(2015),
-        "monthOfYear" -> JsNumber(10),
-        "dayOfMonth" -> JsNumber(25),
-        "hourOfDay" -> JsNumber(expectedHourOfDay),
-        "minuteOfHour" -> JsNumber(25),
-        "secondOfMinute" -> JsNumber(38),
-        "millisOfSecond" -> JsNumber(150)
-      )
-      val dateTime = format.read(json)
-      dateTime.getHourOfDay mustBe expectedHourOfDay
+      val dateTime = jsonToDateTime(TestDateTimeJsonObject)
+      dateTime.getHourOfDay mustBe HourOfDay
     }
 
     it("must read correct minute of hour value") {
-      val format = DateTimeJsonFormat()
-      val expectedMinuteOfHour = 25
-      val json = JsObject(
-        "year" -> JsNumber(2015),
-        "monthOfYear" -> JsNumber(10),
-        "dayOfMonth" -> JsNumber(25),
-        "hourOfDay" -> JsNumber(19),
-        "minuteOfHour" -> JsNumber(expectedMinuteOfHour),
-        "secondOfMinute" -> JsNumber(38),
-        "millisOfSecond" -> JsNumber(150)
-      )
-      val dateTime = format.read(json)
-      dateTime.getMinuteOfHour mustBe expectedMinuteOfHour
+      val dateTime = jsonToDateTime(TestDateTimeJsonObject)
+      dateTime.getMinuteOfHour mustBe MinuteOfHour
     }
 
     it("must read correct second of minute value") {
-      val format = DateTimeJsonFormat()
-      val expectedSecondOfMinute = 38
-      val json = JsObject(
-        "year" -> JsNumber(2015),
-        "monthOfYear" -> JsNumber(10),
-        "dayOfMonth" -> JsNumber(25),
-        "hourOfDay" -> JsNumber(19),
-        "minuteOfHour" -> JsNumber(25),
-        "secondOfMinute" -> JsNumber(expectedSecondOfMinute),
-        "millisOfSecond" -> JsNumber(150)
-      )
-      val dateTime = format.read(json)
-      dateTime.getSecondOfMinute mustBe expectedSecondOfMinute
+      val dateTime = jsonToDateTime(TestDateTimeJsonObject)
+      dateTime.getSecondOfMinute mustBe SecondOfMinute
     }
 
     it("must read correct millis of second value") {
-      val format = DateTimeJsonFormat()
-      val expectedMillisOfSecond = 150
-      val json = JsObject(
-        "year" -> JsNumber(2015),
-        "monthOfYear" -> JsNumber(10),
-        "dayOfMonth" -> JsNumber(25),
-        "hourOfDay" -> JsNumber(19),
-        "minuteOfHour" -> JsNumber(25),
-        "secondOfMinute" -> JsNumber(38),
-        "millisOfSecond" -> JsNumber(expectedMillisOfSecond)
-      )
-      val dateTime = format.read(json)
-      dateTime.getMillisOfSecond mustBe expectedMillisOfSecond
+      val dateTime = jsonToDateTime(TestDateTimeJsonObject)
+      dateTime.getMillisOfSecond mustBe MillisOfSecond
     }
+  }
+}
+
+private object DateTimeJsonFormatSpec extends BasicFormats {
+  lazy val Year: Int = 2015
+  lazy val MonthOfYear: Int = 10
+  lazy val DayOfMonth: Int = 25
+  lazy val HourOfDay: Int = 19
+  lazy val MinuteOfHour: Int = 25
+  lazy val SecondOfMinute: Int = 38
+  lazy val MillisOfSecond: Int = 150
+  lazy val TestDateTime: DateTime = new DateTime(
+    Year,
+    MonthOfYear,
+    DayOfMonth,
+    HourOfDay,
+    MinuteOfHour,
+    SecondOfMinute,
+    MillisOfSecond
+  )
+  lazy val TestDateTimeJsonObject: JsObject = JsObject(
+    "year" -> Year.toJson,
+    "monthOfYear" -> MonthOfYear.toJson,
+    "dayOfMonth" -> DayOfMonth.toJson,
+    "hourOfDay" -> HourOfDay.toJson,
+    "minuteOfHour" -> MinuteOfHour.toJson,
+    "secondOfMinute" -> SecondOfMinute.toJson,
+    "millisOfSecond" -> MillisOfSecond.toJson
+  )
+
+  def dateTimeToJson(dateTime: DateTime): JsObject =
+    DateTimeJsonFormat().write(dateTime).asJsObject
+
+  def jsonToDateTime(json: JsValue): DateTime =
+    DateTimeJsonFormat().read(json)
+
+  implicit class JsObjectWithIntField(jsonObject: JsObject) extends BasicFormats {
+    def intField(fieldName: String): Option[Int] =
+      jsonObject.fields.get(fieldName).map(_.convertTo[Int])
   }
 }
